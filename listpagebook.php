bbd470dfad6511b1d90c623c7fb4d4026f849569
TheJazzyBranco <naimelhammouchi@icloud.com>
2022-02-08 12:30:10 +0100

    listpage verbeteren + php integratie

--- listpagebook.php
+++ listpagebook.php
@@ -6,37 +6,31 @@ CreateConnection();
 <html lang="en">
 <head>
     <?php
-    PrintHead("Home");
+    PrintHead("Boeken");
     ?>
-    <link rel="stylesheet" href="./styles/home.css"/>
+    <link rel="stylesheet" href="./styles/listpage.css"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Old+Standard+TT&display=swap"
-          rel="stylesheet">
+    rel="stylesheet">
 </head>
 <body>
 <?php
 PrintHeader();
 ?>
- <main>
-      <section class="slogan">
-        <div class="container">
-          <p>Proud to be a bookworm</p>
-          <p>Make reading a habit!</p>
-          <p>Welcome, Bookworm! The next chapter's waiting for you!</p>
-        </div>
-      </section>
-      <div class="container">
-        <section class="books">
-          <h2>Popular books</h2>
-          <div class="books__pop">
-                <?php
+
+<main>
+    
+        <div class="lContainer">
+            <section class="listpage">
+                
+        <?php
 
                 // get template for Articles
-                $template = file_get_contents("templates/home.html");
+                $template = file_get_contents("templates/listpagebook.html");
                 
                 // SQL query to get the data from the database
-                $sql = 'SELECT B.book_id, B.book_title, B.book_img, A.aut_id, A.aut_firstname, A.aut_lastname
+                $sql = 'SELECT B.book_id, B.book_title, B.book_img, B.book_pages,  B.publish_year, A.aut_id, A.aut_firstname, A.aut_lastname
                         FROM Book B
                             INNER JOIN Author_Book AB on B.book_id = AB.book_id
                             INNER JOIN Author A on AB.aut_id = A.aut_id
@@ -49,10 +43,10 @@ PrintHeader();
                 print MergeViewWithData($template, $data);
 
                 ?>
-          </div>
+
         </section>
-      </div>
-    </main>
+    </div>
+</main>
 
 <?php
 printFooter();
