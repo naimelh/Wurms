3148d8cb6d51557d9c739108429cf3381699b99e
Michiel Peeters <michiel1.peeters@gmail.com>
2022-02-10 21:41:57 +0100

    Search Filtering and images done

--- search.php
+++ search.php
@@ -6,7 +6,7 @@ CreateConnection();
 <html lang="en">
 <head>
     <?php
-    PrintHead("Home");
+    PrintHead("Search");
     ?>
     <link rel="stylesheet" href="./styles/search.css"/>
     <link rel="preconnect" href="https://fonts.googleapis.com">
@@ -16,73 +16,90 @@ CreateConnection();
 </head>
 <body>
 <?php
-if (key_exists("search",$_GET)){
+if (key_exists("search", $_GET)) {
     PrintHeader($_GET["search"]);
+} else {
+    PrintHeader();
 }
-else{PrintHeader();}
- ?>
+?>
 <div class="container">
     <section class="books">
-        <?php if (!key_exists("search",$_GET) || $_GET["search"] == ""){
+        <?php if (!key_exists("search", $_GET) || $_GET["search"] == "") {
+
+            // get the all the data from Books
+            $sql_book = 'SELECT book_id, book_title, book_img
+                        FROM Book';
             echo "<h2>Books</h2>";
+            $data_book = GetData($sql_book);
+
+            // get all the data for Author
+            $sql_aut = 'SELECT aut_id, aut_firstname, aut_lastname, aut_img
+                        FROM Author';
+            $data_aut = GetData($sql_aut);
 
         } else {
-            echo "<h2>Books that contain ".$_GET["search"]."</h2>";
-        }?>
+            // get all the data for Book
+            $sql_book = 'SELECT book_id, book_title, book_img
+                        FROM Book 
+                        WHERE book_title LIKE "%' . $_GET["search"] . '%"';
+            $data_book = GetData($sql_book);
+            if (count($data_book) > 0) {
+                echo "<h2>Books that contain " . $_GET["search"] . "</h2>";
+            }
+
+            // get all the data for Author
+            $sql_aut = 'SELECT aut_id, aut_firstname, aut_lastname, aut_img
+                        FROM Author 
+                        WHERE aut_firstname LIKE "%' . $_GET["search"] . '%" OR aut_firstname LIKE "%' . $_GET["search"] . '%"';
+            $data_aut = GetData($sql_aut);
+        } ?>
         <div class="books__pop">
             <?php
 
             // get template for Articles
             $template = file_get_contents("templates/search_book.html");
 
-            // SQL query to get the data from the database
-            if (key_exists("search",$_GET)){
-                $sql = 'SELECT book_id, book_title, book_img
-                        FROM Book 
-                        WHERE book_title LIKE "%'.$_GET["search"].'%"';
-            }else {
-                $sql = 'SELECT book_id, book_title, book_img
-                        FROM Book';
+
+            if (count($data_book) > 0) {
+                // Merge the Template of Article with the data and fill in the @..@ positions
+                print MergeViewWithData($template, $data_book);
             }
-
-
-            // get the data
-            $data = GetData($sql);
-
-            // Merge the Template of Article with the data and fill in the @..@ positions
-            print MergeViewWithData($template, $data);
 
             ?>
         </div>
     </section>
     <section class="books">
-        <?php if (!key_exists("search",$_GET) || $_GET["search"] == ""){
+        <?php if (!key_exists("search", $_GET) || $_GET["search"] == "") {
+
             echo "<h2>Author</h2>";
 
         } else {
-            echo "<h2>Author that contain ".$_GET["search"]."</h2>";
-        }?>
+            if (count($data_aut) > 0) {
+                echo "<h2>Author that contain " . $_GET["search"] . "</h2>";
+            }
+        } ?>
         <div class="books__pop">
             <?php
 
             // get template for Articles
             $template = file_get_contents("templates/search_author.html");
 
-            // SQL query to get the data from the database
-            if (key_exists("search",$_GET)){
-                $sql = 'SELECT aut_id, aut_firstname, aut_lastname, aut_img
-                        FROM Author 
-                        WHERE aut_firstname LIKE "%'.$_GET["search"].'%" OR aut_firstname LIKE "%'.$_GET["search"].'%"';
-            }else {
-                $sql = 'SELECT aut_id, aut_firstname, aut_lastname, aut_img
-                        FROM Author';
+
+            if (count($data_aut) > 0) {
+                // Merge the Template of Article with the data and fill in the @..@ positions
+                print MergeViewWithData($template, $data_aut);
             }
-            // get the data
-            $data = GetData($sql);
 
-            // Merge the Template of Article with the data and fill in the @..@ positions
-            print MergeViewWithData($template, $data);
 
+            ?>
+        </div>
+    </section>
+    <section class="books">
+        <div class="books__pop">
+            <?php
+                if(count($data_aut) === 0 && count($data_book) === 0) {
+                    echo "<h2> No Results Found! </h2>";
+                }
             ?>
         </div>
     </section>
