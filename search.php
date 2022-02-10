be9c33ca18eeaa4c32fc379fcb7096eeab33154a
Michiel Peeters <michiel1.peeters@gmail.com>
2022-02-10 22:07:45 +0100

    Comments added

--- search.php
+++ search.php
@@ -24,7 +24,9 @@ if (key_exists("search", $_GET)) {
 ?>
 <div class="container">
     <section class="books">
-        <?php if (!key_exists("search", $_GET) || $_GET["search"] == "") {
+        <?php
+        // Checking if GET has search of if search is an empty string
+        if (!key_exists("search", $_GET) || $_GET["search"] == "") {
 
             // get the all the data from Books
             $sql_book = 'SELECT book_id, book_title, book_img
@@ -38,7 +40,7 @@ if (key_exists("search", $_GET)) {
             $data_aut = GetData($sql_aut);
 
         } else {
-            // get all the data for Book
+            // get all the data that is = to the search value for Book
             $sql_book = 'SELECT book_id, book_title, book_img
                         FROM Book 
                         WHERE book_title LIKE "%' . $_GET["search"] . '%"';
@@ -47,7 +49,7 @@ if (key_exists("search", $_GET)) {
                 echo "<h2>Books that contain " . $_GET["search"] . "</h2>";
             }
 
-            // get all the data for Author
+            // get all the data that is = to the search value for Book
             $sql_aut = 'SELECT aut_id, aut_firstname, aut_lastname, aut_img
                         FROM Author 
                         WHERE aut_firstname LIKE "%' . $_GET["search"] . '%" OR aut_firstname LIKE "%' . $_GET["search"] . '%"';
@@ -59,7 +61,7 @@ if (key_exists("search", $_GET)) {
             // get template for Articles
             $template = file_get_contents("templates/search_book.html");
 
-
+            // only print the template when you have min. 1 row
             if (count($data_book) > 0) {
                 // Merge the Template of Article with the data and fill in the @..@ positions
                 print MergeViewWithData($template, $data_book);
@@ -69,13 +71,15 @@ if (key_exists("search", $_GET)) {
         </div>
     </section>
     <section class="books">
-        <?php if (!key_exists("search", $_GET) || $_GET["search"] == "") {
+        <?php
+        // Checking if GET has search of if search is an empty string
+        if (!key_exists("search", $_GET) || $_GET["search"] == "") {
 
-            echo "<h2>Author</h2>";
+            echo "<h2>Authors</h2>";
 
         } else {
             if (count($data_aut) > 0) {
-                echo "<h2>Author that contain " . $_GET["search"] . "</h2>";
+                echo "<h2>Authors that contain " . $_GET["search"] . "</h2>";
             }
         } ?>
         <div class="books__pop">
@@ -84,7 +88,7 @@ if (key_exists("search", $_GET)) {
             // get template for Articles
             $template = file_get_contents("templates/search_author.html");
 
-
+            // only print the template when you have min. 1 row
             if (count($data_aut) > 0) {
                 // Merge the Template of Article with the data and fill in the @..@ positions
                 print MergeViewWithData($template, $data_aut);
@@ -97,6 +101,7 @@ if (key_exists("search", $_GET)) {
     <section class="books">
         <div class="books__pop">
             <?php
+                // both has no results print No results
                 if(count($data_aut) === 0 && count($data_book) === 0) {
                     echo "<h2> No Results Found! </h2>";
                 }
