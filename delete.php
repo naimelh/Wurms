5fea935298e8139f427bfd8cdc548d726900f807
Magdalena Sarandeva <91531129+magdalenasar@users.noreply.github.com>
2022-02-14 16:47:12 +0100

    Update delete.php

--- delete.php
+++ delete.php
@@ -3,42 +3,76 @@ require_once "lib/autoload.php";
 CreateConnection();
 ?>
 
-<!DOCTYPE html>
-<html lang="en">
 
-<head>
-    <link rel="stylesheet" href="./styles/delete.css" />
-    <link rel="preconnect" href="https://fonts.googleapis.com">
-    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
-    <link href="https://fonts.googleapis.com/css2?family=Old+Standard+TT&display=swap" rel="stylesheet">
-</head>
-<div class="dContainer">
+DeleteFormData();
+
+function DeleteFormData()
+{
+    if ($_SERVER['REQUEST_METHOD'] == "POST") {
+
+        //controle CSRF token
+        if (!key_exists("csrf", $_POST)) die("Missing CSRF");
+        if (!hash_equals($_POST['csrf'], $_SESSION['lastest_csrf'])) die("Problem with CSRF");
+
+        $_SESSION['lastest_csrf'] = "";
+
+        //sanitization
+        $_POST = StripSpaces($_POST);
+        $_POST = ConvertSpecialChars($_POST);
+
+        $table = $pkey = $delete = $where = $str_keys_values = "";
+
+        //get important metadata
+        if (!key_exists("table", $_POST)) die("Missing table");
+        if (!key_exists("pkey", $_POST)) die("Missing pkey");
+
+        $table = $_POST['table'];
+        $pkey = $_POST['pkey'];
+        $book = $_POST['id'];
+        $author = $_POST['id'];
+
+        //validation
+        $sending_form_uri = $_SERVER['HTTP_REFERER'];
+        CompareWithDatabase($table, $pkey);
+
+        //terugkeren naar afzender als er een fout is
+        if (array_key_exists('errors', $_SESSION) && $_SESSION["errors"] != null && count($_SESSION['errors']) > 0 ) {
+            header("Location: " . $sending_form_uri);
+            exit();
+
+        }
+
+        //delete?
+        if ( $_POST["$pkey"] > 0 ) $delete = true;
+
+        //delete query
+        $sql = "DELETE $table SET ";
 
 
-    <?php
-    //  TEST
-    // if (isset($_POST['id']) && is_numeric($_POST['id'])) {
-    // $aut_id = $_POST['aut_id'];
-    // $sql = "DELETE FROM author WHERE aut_id = $aut_id";
-    // or die(mysql_error());
-    //  echo "OK";
-    //  } else {
-    //  header("Location: home.php"); } 
+        foreach ($_POST as $field => $value) {
+            //skip non-data fields
+            if (in_array($field, ['table', 'pkey', 'afterinsert', 'afterupdate', 'csrf', 'afterdelete'])) continue;
 
+            //handle primary key field
+            if ($field == $pkey) {
+                if ($delete) $where = " WHERE $pkey = $value ";
+                continue;
+            }
 
-    //Define the query to delete a record
-    $sql = "DELETE FROM author WHERE name = {$_POST['delete']} LIMIT 1";
+        }
 
-    //sends the query to delete the entry
-    // on success delete : redirect the page to original page using header() method
-    if ($conn->query($sql) === TRUE) {
-        header('Location: index.php'); //or echo "<div class="deleted"><h2 >Author has been deleted </h2></div><br /><br />" 
-        exit;
-    } else {
-        //if it failed
-        echo 'div class="failed"><h2>' . "Deletion Failed" . '</h2></div><br /><br />';
+        //extend SQL with WHERE
+        $sql .= $where;
+
+        //run SQL
+        $result = ExecuteSQL($sql);
+
+        //output if not redirected
+        print $sql;
+        print "<br>";
+        print $result->rowCount() . " records affected";
+
+        //redirect after insert or update
+        if ($delete and $_POST["afterdelete"] > "") header("Location: ../" . $_POST["afterdelete"] );
     }
-    mysqli_close($conn);
-    ?>
-
-</div>
+}
