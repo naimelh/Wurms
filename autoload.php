481e70993423ac5163e49b9428fbccdef0456805
Magdalena Sarandeva <91531129+magdalenasar@users.noreply.github.com>
2022-02-15 09:53:25 +0100

    Update autoload.php

--- autoload.php
+++ autoload.php
@@ -1,7 +1,7 @@
 <?php
 session_start();
 
-require_once "connection_data.php";
+require_once "config.php";
 require_once "pdo.php";
 require_once "html_functions.php";
 require_once "form_elements.php";
